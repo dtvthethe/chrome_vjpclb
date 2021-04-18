22b5a9ac98e0fb90a8c1a7833a9b0d6f917535f8
Đàm Thế <damthe@ams-MacBook-Pro.local>
2021-04-19 01:44:52 +0700

    Init

---
+++ content.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $('.candich').css('pointer-events', 'none');
+    $('.kqdich').css({
+        'display': 'contents',
+    });
+    $('.kqdich .nddich').css({
+        'background-color': '#ffffff',
+        'font-style': 'italic',
+        'width': '100%',
+        'padding': '0px'
+    });
+    $('.slide .slide-content').css('display', 'block');
+});
+
+ 
